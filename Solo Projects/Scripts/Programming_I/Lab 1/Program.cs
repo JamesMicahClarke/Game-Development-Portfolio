8b9cc1a62750ea03ebb069149871b17a76ac2917
James Clarke <91476897+Snowboyman@users.noreply.github.com>
2023-12-19 14:31:45 -0500

    Solo Projects: Programming 1

---
+++ Solo Projects/Scripts/Programming_I/Lab 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Full Sail!");
+            Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("My name is " + name + ".");
+            Console.ReadLine();
+            Console.WriteLine("What is your age " + name + "?");
+            string answer = Console.ReadLine(); 
+            Console.WriteLine(name + " is " + answer + " years old.");
+            Console.WriteLine("Welcome to Full Sail " + name + "! You are awesome for being " + answer + " years old!");
+            Console.ReadKey();
+            int x = 5;
+            Add10(x);
+            Console.Write(x);
+
+        }
+        static void Add10(int x)
+        {
+            x += 10;
+        }
+    }
+}
